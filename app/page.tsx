f749623e124f8b99f1a122af12dce3facfc44a7d
aikoo7 <prozinhopro1973@gmail.com>
2023-09-07 02:38:43 +0100

    Fixed the credits.
    Signed-off-by: aikoo7 <prozinhopro1973@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div className="Hello">
       <h1>Welcome to the homepage of my note system</h1>
       <p style={{textAlign: 'center'}}>Click on the notes button to have the experience 🤑🤑🤑</p>
-      <p className="credits">Done by aikooo7, <a href="https://github.com/aikooo7?tab=repositories">github.</a></p>
+      <p className="credits">Done by aikooo7, <a href="https://github.com/aikooo7">github.</a></p>
     </div>
   );
 }
