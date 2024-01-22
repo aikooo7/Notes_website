44eca1481b0bb7b398a2782282feeed7574505fa
aikooo7 <prozinhopro1973@gmail.com>
2024-01-22 00:16:39 +0000

    style: indenting and semi-colomns

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import styles from './Notes.module.css';
 import prisma from '../../lib/prisma'
+import CreateNote from "./Notes";
 
 export default async function Notes() {
   const notes = await prisma.notes.findMany();
@@ -13,6 +14,8 @@ export default async function Notes() {
           gridTemplateColumns: "auto auto auto",
         }}
       >
+        {notes && notes.length > 0 ? (
+          notes.map((note: any) => <Note key={note.id} note={note} />)
         ) : (
           <li>No notes found</li>
         )}
@@ -23,7 +26,7 @@ export default async function Notes() {
 }
 
 function Note({ note }: any) {
-  const {id, title, notes_content, createdAt} = note || {}
+  const { id, title, notes_content, createdAt } = note || {};
   const formattedDate = new Date(note.createdAt).toLocaleDateString();
 
   return (
